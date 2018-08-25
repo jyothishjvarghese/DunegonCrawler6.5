d36a17f2f177e4f8c55086aa238c8f7acffbd074
Jyothish Varghese <jyothish.j.varghese@gmail.com>
2018-08-25 10:09:49 -0400

    Version1.3
    hope thisworks

---
+++ DungeonCrawler6.5/Assets/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    protected Animator _anim;
+
+	void Start ()
+    {
+        _anim = GetComponentInChildren<Animator>();
+	}
+	
+    public void Run()
+    {
+
+    }
+	
+}
